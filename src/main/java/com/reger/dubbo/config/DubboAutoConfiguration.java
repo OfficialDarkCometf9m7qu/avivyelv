30fbb967e92156a25fec5d456f9dc83c1d641685
OfficialDarkCometf9m7qu <wcshh7@163.com>
2022-07-05 20:19:42 +0800

    "

--- src/main/java/com/reger/dubbo/config/DubboAutoConfiguration.java
+++ src/main/java/com/reger/dubbo/config/DubboAutoConfiguration.java
@@ -270,7 +270,7 @@ public class DubboAutoConfiguration extends AnnotationBean
 	private void registerThis(String basePackages, ConfigurableListableBeanFactory beanFactory) {
 		super.setId("dubboBasePackages");
 		if (StringUtils.isEmpty(basePackages)) {
-			logger.info("dubbo没有配置注解服务所在的包路径,如果你使用注解扫描，该日志可忽略。");
+//			logger.info("dubbo没有配置注解服务所在的包路径,如果你使用注解扫描，该日志可忽略。");
 		} else {
 			logger.info("dubbo开始扫描: {}", basePackages);
 		}
